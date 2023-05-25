4015dfeabb9d5ba1eb0af998d70b7dc96d857da5
Felix Hellman <felix@helagon.se>
2023-05-25 14:55:16 +0200

    Adds padding to banner images

--- bankid-idp/bankid-idp-frontend/src/components/Header.vue
+++ bankid-idp/bankid-idp-frontend/src/components/Header.vue
@@ -3,10 +3,10 @@
         <center>
             <div class="row">
                 <div class="column">
-                    <img class="top-logo-dim float-left" src="@/assets/logo.svg" alt="Logo"/>
+                    <img class="top-logo-dim" src="@/assets/logo.svg" alt="Logo"/>
                 </div>
                 <div class="column">
-                    <img class="top-logo-dim float-right" src="@/assets/BankID_logo.svg"
+                    <img class="top-logo-dim" src="@/assets/BankID_logo.svg"
                          alt="Logo"/>
                 </div>
             </div>
