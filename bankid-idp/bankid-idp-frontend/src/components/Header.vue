c2c0b3d640872f59d25f1f38f6982d07655d0c08
Felix Hellman <felix@helagon.se>
2023-05-25 13:55:59 +0200

    Header banner fills page

--- bankid-idp/bankid-idp-frontend/src/components/Header.vue
+++ bankid-idp/bankid-idp-frontend/src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="container header">
-    <div class="col-sm-12 top-logo">
-      <div class="row">
-        <div class="column">
-          <img class="top-logo-dim float-left" src="@/assets/logo.svg" alt="Logo"/>
-        </div>
-        <div class="column">
-          <img class="top-logo-dim float-right" src="@/assets/BankID_logo.svg"
-               alt="Logo"/>
-        </div>
-      </div>
+    <div class="container-fluid header">
+        <center>
+            <div class="row">
+                <div class="column">
+                    <img class="top-logo-dim float-left" src="@/assets/logo.svg" alt="Logo"/>
+                </div>
+                <div class="column">
+                    <img class="top-logo-dim float-right" src="@/assets/BankID_logo.svg"
+                         alt="Logo"/>
+                </div>
+            </div>
+        </center>
+        <hr>
     </div>
-    <hr>
-  </div>
 </template>
