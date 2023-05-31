da91ddd27bcaa87822e587c324e935d77603e8f0
Felix Hellman <felix@helagon.se>
2023-05-31 14:57:51 +0200

    Adds language selector

--- bankid-idp/bankid-idp-frontend/src/App.vue
+++ bankid-idp/bankid-idp-frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import Localechanger from "@/components/LanguageSelector.vue";
 </script>
 
 <template>
 
   <Header/>
+  <Localechanger/>
   <RouterView/>
   <Footer/>
 </template>
---
+++ bankid-idp/bankid-idp-frontend/src/components/LanguageSelector.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container main" id="languageDiv">
+        <div class="col-sm-12">
+            <select v-model="$i18n.locale">
+                <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">
+                    {{ lang }}
+                </option>
+            </select>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'localechanger',
+    data() {
+        return { langs: ['sv', 'en'] };
+    },
+};
+</script>
