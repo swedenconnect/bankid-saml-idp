e4ee4200d03588c494462e6ae7e819a19a3c79e8
Felix Hellman <felix@helagon.se>
2023-08-11 13:51:43 +0200

    Adds test for CustomerContactInformationFactory

--- bankid-idp/bankid-idp-backend/src/test/java/se/swedenconnect/bankid/idp/authn/CustomerContactInformationFactoryTest.java
+++ bankid-idp/bankid-idp-backend/src/test/java/se/swedenconnect/bankid/idp/authn/CustomerContactInformationFactoryTest.java
@@ -1,5 +1,6 @@
 package se.swedenconnect.bankid.idp.authn;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerContactInformationFactoryTest {
   @Test
   void customerContactInfoHiddenWhenConfigured() {
-    CustomerContactInformationFactory customerContactInformationFactory = new CustomerContactInformationFactory(new UserErrorProperties("", false, false));
+    CustomerContactInformationFactory customerContactInformationFactory = new CustomerContactInformationFactory(UserErrorPropertiesFixture.EMPTY_PROPERTIES);
+    CustomerContactInformation contactInformation = customerContactInformationFactory.getContactInformation();
+    Assertions.assertEquals("", contactInformation.getEmail());
+    Assertions.assertEquals(false, contactInformation.getDisplayInformation());
+  }
+
+  @Test
+  void customerContactInfoShowWhenConfigured() {
+    CustomerContactInformationFactory customerContactInformationFactory = new CustomerContactInformationFactory(UserErrorPropertiesFixture.SHOW_EMAIL_NO_TRACE);
+    CustomerContactInformation contactInformation = customerContactInformationFactory.getContactInformation();
+    Assertions.assertEquals(UserErrorPropertiesFixture.SHOW_EMAIL_NO_TRACE.getContactEmail(), contactInformation.getEmail());
+    Assertions.assertEquals(true, contactInformation.getDisplayInformation());
   }
 
 }
