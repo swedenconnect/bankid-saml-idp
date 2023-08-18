6004b3cdbaa3d9640ac5ac0f6ef1619ec3bbf0a2
Felix Hellman <felix@helagon.se>
2023-08-18 08:00:08 +0200

    Refactor creation of data objects in test

--- bankid-idp/bankid-idp-backend/src/test/java/se/swedenconnect/bankid/idp/authn/service/BankIdServiceTest.java
+++ bankid-idp/bankid-idp-backend/src/test/java/se/swedenconnect/bankid/idp/authn/service/BankIdServiceTest.java
@@ -38,64 +38,49 @@ class BankIdServiceTest {
 
   @Test
   void pollAndReInit() {
-    // First request should trigger a authenticate and then collect
-    BankIDClient client = Mockito.mock(BankIDClient.class);
     ApplicationEventPublisher publisher = Mockito.mock(ApplicationEventPublisher.class);
     CircuitBreaker circuitBreaker = Mockito.mock(CircuitBreaker.class);
     when(circuitBreaker.tryAcquirePermission()).thenReturn(true);
-    OrderResponse data1 = new OrderResponse();
-    data1.setQrStartToken("qrs-1");
-    data1.setOrderReference("or-1");
-    data1.setAutoStartToken("ast-1");
-    data1.setQrStartSecret("qss-1");
-    OrderResponse data2 = new OrderResponse();
-    data1.setQrStartToken("qrs-2");
-    data1.setOrderReference("or-2");
-    data1.setAutoStartToken("ast-2");
-    data1.setQrStartSecret("qss-2");
-    when(client.authenticate(any())).thenReturn(Mono.just(data1)).thenReturn(Mono.just(data2));
-
-    when(client.collect(any())).thenAnswer(a -> {
-      CollectResponse collectResponse = new CollectResponse();
-      collectResponse.setOrderReference(data1.getOrderReference());
-      collectResponse.setHintCode("hint");
-      collectResponse.setStatus(CollectResponse.Status.PENDING);
-      return Mono.just(collectResponse);
-    });
     BankIdService service = new BankIdService(new BankIdEventPublisher(publisher), circuitBreaker, new BankIdRequestFactory());
-    BankIdContext context = new BankIdContext();
-    context.setOperation(BankIdOperation.AUTH);
-    HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-    when(request.getRemoteAddr()).thenReturn("1.1.1.1");
-    PollRequest pollRequest1 = PollRequest.builder().qr(false).request(request).context(context).relyingPartyData(new RelyingPartyData(client, List.of("asd"), new DisplayText(), new DisplayText(), new EntityRequirement())).build();
+    BankIDClient client = Mockito.mock(BankIDClient.class);
+    OrderResponse expectedFirstSession = BankIdResponseFixture.createOrderResponse(1);
+    OrderResponse expectedSecondSession = BankIdResponseFixture.createOrderResponse(2);
+    CollectResponse initialCollect = BankIdResponseFixture.createInitial(expectedFirstSession);
+
+    when(client.authenticate(any())).thenReturn(Mono.just(expectedFirstSession)).thenReturn(Mono.just(expectedSecondSession));
+    when(client.collect(any())).thenAnswer(a -> {
+      return Mono.just(initialCollect);
+    });
+    
+    // First request should trigger a authenticate and then collect
+    // ---
+    PollRequest pollRequest1 = BankIdResponseFixture.createPollRequest(client);
     ApiResponse response1 = service.poll(pollRequest1).block();
     Assertions.assertNotNull(response1);
-    Assertions.assertEquals(response1.getAutoStartToken(), data1.getAutoStartToken());
+    Assertions.assertEquals(response1.getAutoStartToken(), expectedFirstSession.getAutoStartToken());
     Mockito.verify(client, times(1)).authenticate(any());
     Mockito.verify(client, times(1)).collect(any());
-    BankIdSessionState sessionState = new BankIdSessionState();
-    sessionState.push(BankIdSessionData.of(pollRequest1, data1));
+    BankIdSessionState sessionState = BankIdResponseFixture.create(pollRequest1, expectedFirstSession);
+
     // Second request should trigger collect
-    ApiResponse response2 = service.poll(PollRequest.builder().state(sessionState).qr(false).request(request).context(context).relyingPartyData(new RelyingPartyData(client, List.of("asd"), new DisplayText(), new DisplayText(), new EntityRequirement())).build()).block();
+    // ---
+    ApiResponse response2 = service.poll(BankIdResponseFixture.createPollrequest(client, sessionState)).block();
     Mockito.verify(client, times(1)).authenticate(any());
     Mockito.verify(client, times(2)).collect(any());
     Assertions.assertNotNull(response2);
-    Assertions.assertEquals(response2.getAutoStartToken(), data1.getAutoStartToken());
-    CollectResponse collectResponse = new CollectResponse();
-    collectResponse.setStatus(CollectResponse.Status.FAILED);
-    collectResponse.setOrderReference("2");
-    collectResponse.setHintCode(ErrorCode.START_FAILED.getValue());
+    Assertions.assertEquals(response2.getAutoStartToken(), expectedFirstSession.getAutoStartToken());
+    CollectResponse collectResponse = BankIdResponseFixture.createStartFailed(initialCollect);
     Mockito.when(client.collect(any())).thenAnswer(a -> {
       return Mono.just(collectResponse);
     });
-    BankIdSessionData newState = BankIdSessionData.of(sessionState.getBankIdSessionData(), collectResponse);
-    sessionState = new BankIdSessionState();
-    sessionState.push(newState);
+    BankIdResponseFixture.update(sessionState, collectResponse);
+
     // Third request should trigger authenticate and collect again with a new autostartToken
-    ApiResponse response3 = service.poll(PollRequest.builder().qr(false).request(request).state(sessionState).context(context).relyingPartyData(new RelyingPartyData(client, List.of("asd"), new DisplayText(), new DisplayText(), new EntityRequirement())).build()).block();
+    // ---
+    ApiResponse response3 = service.poll(BankIdResponseFixture.createPollrequest(client, sessionState)).block();
     Mockito.verify(client, times(2)).authenticate(any());
     Mockito.verify(client, times(3)).collect(any());
     Assertions.assertNotNull(response3);
-    Assertions.assertEquals(response3.getAutoStartToken(), data2.getAutoStartToken());
+    Assertions.assertEquals(response3.getAutoStartToken(), expectedSecondSession.getAutoStartToken());
   }
 }
