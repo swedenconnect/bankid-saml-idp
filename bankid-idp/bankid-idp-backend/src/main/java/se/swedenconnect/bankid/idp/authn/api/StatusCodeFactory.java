67803a45db0081ac902526a0ebfb6f9ac335e767
Felix Hellman <felix@helagon.se>
2023-08-22 07:21:14 +0200

    Fixes Nullpointer exception

--- bankid-idp/bankid-idp-backend/src/main/java/se/swedenconnect/bankid/idp/authn/api/StatusCodeFactory.java
+++ bankid-idp/bankid-idp-backend/src/main/java/se/swedenconnect/bankid/idp/authn/api/StatusCodeFactory.java
@@ -23,6 +23,7 @@ import se.swedenconnect.bankid.rpapi.types.ProgressStatus;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -36,7 +37,7 @@ public class StatusCodeFactory {
       c -> PENDING.equals(c.getCollectResponse().getStatus()) && List.of(ProgressStatus.OUTSTANDING_TRANSACTION, ProgressStatus.NO_CLIENT).contains(c.getCollectResponse().getProgressStatus()), "rfa1",
       c -> ErrorCode.CANCELLED.equals(c.getCollectResponse().getErrorCode()), "rfa3",
       c -> ErrorCode.ALREADY_IN_PROGRESS.equals(c.getCollectResponse().getErrorCode()), "rfa4",
-      c -> List.of(ErrorCode.REQUEST_TIMEOUT, ErrorCode.MAINTENANCE, ErrorCode.INTERNAL_ERROR).contains(c.getCollectResponse().getErrorCode()), "rfa5",
+      c -> Objects.nonNull(c.getCollectResponse().getErrorCode()) && List.of(ErrorCode.REQUEST_TIMEOUT, ErrorCode.MAINTENANCE, ErrorCode.INTERNAL_ERROR).contains(c.getCollectResponse().getErrorCode()), "rfa5",
       c -> FAILED.equals(c.getCollectResponse().getStatus()) && ProgressStatus.NO_CLIENT.equals(c.getCollectResponse().getProgressStatus()), "rfa6",
       c -> FAILED.equals(c.getCollectResponse().getStatus()) && ProgressStatus.EXPIRED_TRANSACTION.equals(c.getCollectResponse().getProgressStatus()), "rfa8",
       c -> PENDING.equals(c.getCollectResponse().getStatus()) && ProgressStatus.USER_SIGN.equals(c.getCollectResponse().getProgressStatus()), "rfa9",
@@ -54,7 +55,7 @@ public class StatusCodeFactory {
 
   public static String statusCode(CollectResponse json, Boolean showQr) {
     Stream<Map.Entry<Predicate<StatusData>, String>> qrMessageStream = QR_MESSAGE_CONDITIONS.entrySet().stream();
-    Stream<Map.Entry<Predicate<StatusData>, String>> messageStream = MESSAGE_CONDITIONS.entrySet().stream();
+    Stream<Map.Entry<Predicate<StatusData>, String>> messageStream =  MESSAGE_CONDITIONS.entrySet().stream();
     Optional<String> message = Stream.concat(qrMessageStream, messageStream)
         .filter(kv -> kv.getKey().test(new StatusData(json, showQr)))
         .map(Map.Entry::getValue)
