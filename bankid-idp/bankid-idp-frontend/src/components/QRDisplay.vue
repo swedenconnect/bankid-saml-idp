89c19430ef2ad5b0ccc835240509f99f9da14d97
Mattias Kesti <oestrogen@gmail.com>
2023-08-29 10:11:01 +0200

    Get rid of center element in QRDisplay component

--- bankid-idp/bankid-idp-frontend/src/components/QRDisplay.vue
+++ bankid-idp/bankid-idp-frontend/src/components/QRDisplay.vue
@@ -5,16 +5,16 @@
 </script>
 
 <template>
-  <div v-if="props.image">
+  <div v-if="props.image" class="qr-code">
     <hr class="full-width" />
-    <center>
-      <!-- TODO center with css -->
-      <img width="165" height="165" :src="props.image" alt="QR Code" />
-    </center>
+    <img width="165" height="165" :src="props.image" alt="QR Code" />
   </div>
 </template>
 
 <style scoped>
+  .qr-code {
+    text-align: center;
+  }
   img {
     margin-top: 25px;
   }
