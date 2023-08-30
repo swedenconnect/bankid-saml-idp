69109b1e27522e36475886857d2ad05e779ce92f
Mattias Kesti <oestrogen@gmail.com>
2023-08-30 13:42:20 +0200

    Add support for Typescript

--- bankid-idp/bankid-idp-frontend/vite.config.ts
+++ bankid-idp/bankid-idp-frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    // ...
-  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
