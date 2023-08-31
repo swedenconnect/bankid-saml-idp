f1f3a38857e4e0c05ba816bf9b92954031e72df2
Mattias Kesti <oestrogen@gmail.com>
2023-08-31 12:25:00 +0200

    Convert App to Typescript

--- bankid-idp/bankid-idp-frontend/src/App.vue
+++ bankid-idp/bankid-idp-frontend/src/App.vue
@@ -1,11 +1,12 @@
-<script setup>
+<script setup lang="ts">
   import { onMounted, ref } from 'vue';
   import AppFooter from '@/components/AppFooter.vue';
   import AppHeader from '@/components/AppHeader.vue';
   import LocaleChanger from '@/components/LocaleChanger.vue';
   import { spInformation } from '@/Service';
+  import type { SpInformation } from '@/types';
 
-  const spInfo = ref(null);
+  const spInfo = ref<SpInformation | null>(null);
 
   onMounted(async () => {
     spInfo.value = await spInformation();
