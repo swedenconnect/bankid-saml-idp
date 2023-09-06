a75d6ccd0318b1644bfa1b4952da76f52cd64536
Mattias Kesti <oestrogen@gmail.com>
2023-09-06 13:40:18 +0200

    Refactor SignMessage

--- bankid-idp/bankid-idp-frontend/src/components/SignMessage.vue
+++ bankid-idp/bankid-idp-frontend/src/components/SignMessage.vue
@@ -5,12 +5,24 @@
 </script>
 
 <template>
-  <div class="full-width sign-message">
-    <div class="row no-gutters">
-      <div class="col">
-        <span>{{ props.signMessage }}</span>
-      </div>
+  <div class="sign-message">
+    <div>
+      <span>{{ props.signMessage }}</span>
     </div>
   </div>
   <input type="hidden" name="signMessageDisplayed" />
 </template>
+
+<style scoped>
+  .sign-message {
+    margin-bottom: 30px;
+    background-color: #f7f7f7;
+    border: 1px solid #d0caca;
+    padding: 23px 28px;
+  }
+
+  .sign-message div {
+    margin: 0;
+    font-family: SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace;
+  }
+</style>
