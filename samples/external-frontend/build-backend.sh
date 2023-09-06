b7efd234018bbff0361726328b079f6bb0d0fb7b
Felix Hellman <felix@helagon.se>
2023-09-06 07:45:34 +0200

    Adds support for building backend container for arm platform

--- samples/external-frontend/build-backend.sh
+++ samples/external-frontend/build-backend.sh
@@ -1,2 +1,9 @@
 mvn clean install -f ../../bankid-idp/pom.xml -DskipTests=true
-DOCKER_REPO='local.dev.swedenconnect.se' mvn clean compile jib:dockerBuild -f ../../bankid-idp/bankid-idp-backend/pom.xml -Djib.from.platforms=linux/arm64 -Djib.from.image=arm64v8/openjdk:17 \
+
+if [ `uname -m` = "arm64" ]; then
+    DOCKER_REPO='local.dev.swedenconnect.se' mvn clean compile jib:dockerBuild -f ../../bankid-idp/bankid-idp-backend/pom.xml \
+	-Djib.from.platforms=linux/arm64\
+	-Djib.from.image=arm64v8/openjdk:17
+else
+    DOCKER_REPO='local.dev.swedenconnect.se' mvn clean compile jib:dockerBuild -f ../../bankid-idp/bankid-idp-backend/pom.xml
+fi
