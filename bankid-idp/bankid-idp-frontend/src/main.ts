5e39d51714f811b61ef44f1959429e6c4522d8bb
Mattias Kesti <extern.mattias.kesti@digg.se>
2023-09-12 22:52:06 +0200

    Add style overrides

--- bankid-idp/bankid-idp-frontend/src/main.ts
+++ bankid-idp/bankid-idp-frontend/src/main.ts
@@ -3,21 +3,33 @@ import './assets/open-sans-fonts.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createI18n } from 'vue-i18n';
+import { getOverrides } from '@/Service';
 import App from './App.vue';
 import { messages } from './locale/messages';
 import router from './router';
 
-const i18n = createI18n({
-  legacy: false,
-  locale: 'sv',
-  fallbackLocale: 'en',
-  messages,
-});
+async function initializeApp() {
+  const overrides = await getOverrides();
+  if (overrides.css.length > 0) {
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(overrides.css[0].style));
+    document.head.append(style);
+  }
 
-const app = createApp(App);
-const pinia = createPinia();
-app.use(pinia);
-app.use(router);
-app.use(i18n);
+  const i18n = createI18n({
+    legacy: false,
+    locale: 'sv',
+    fallbackLocale: 'en',
+    messages,
+  });
 
-app.mount('#app');
+  const app = createApp(App);
+  const pinia = createPinia();
+  app.use(pinia);
+  app.use(router);
+  app.use(i18n);
+
+  app.mount('#app');
+}
+
+initializeApp();
