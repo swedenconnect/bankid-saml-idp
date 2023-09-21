b726ef39e6954d5466c0163d2dbd595848716afd
Mattias Kesti <extern.mattias.kesti@digg.se>
2023-09-21 11:46:21 +0200

    Move footer logo to the right

--- bankid-idp/src/main/frontend/src/components/AppFooter.vue
+++ bankid-idp/src/main/frontend/src/components/AppFooter.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
-
-const CONTEXT_PATH = import.meta.env.BASE_URL;
-const LOGO_PATH = CONTEXT_PATH + "/logo.svg";
+  const CONTEXT_PATH = import.meta.env.BASE_URL;
+  const LOGO_PATH = CONTEXT_PATH + '/logo.svg';
 </script>
 <template>
   <footer class="main-width">
+    <img class="logo" :src="LOGO_PATH" alt="Logo" />
     <p class="copyright">{{ $t('bankid.msg.copyright') }}</p>
-    <img height="16" :src=LOGO_PATH alt="Logo"/>
   </footer>
 </template>
 
 <style scoped>
   footer {
     display: flex;
+    flex-direction: column-reverse;
     align-items: center;
-    justify-content: end;
     gap: 10px;
+    border-top: 1px solid rgba(32, 0, 0, 0.18);
+    padding: 1.5em 0.5em;
+  }
+  .logo {
+    max-height: 40px;
+    max-width: 300px;
   }
   .copyright {
     font-size: 10px;
+    text-align: center;
+  }
+  @media (min-width: 576px) {
+    footer {
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .copyright {
+      text-align: right;
+    }
   }
 </style>
