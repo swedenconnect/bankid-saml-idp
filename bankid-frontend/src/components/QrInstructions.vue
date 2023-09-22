c9a7a12bad27fa9c21bb8581ef7feb2bf487b38a
Mattias Kesti <extern.mattias.kesti@digg.se>
2023-09-22 16:16:52 +0200

    Add QR instructions component

---
+++ bankid-frontend/src/components/QrInstructions.vue
@@ -0,0 +1,9 @@
+<template>
+  <ol>
+    <li>{{ $t('bankid.msg.qr-instructions.qr1') }}</li>
+    <li>{{ $t('bankid.msg.qr-instructions.qr2') }}</li>
+    <li>{{ $t('bankid.msg.qr-instructions.qr3') }}</li>
+    <li>{{ $t('bankid.msg.qr-instructions.qr4') }}</li>
+    <li>{{ $t('bankid.msg.qr-instructions.qr5') }}</li>
+  </ol>
+</template>
