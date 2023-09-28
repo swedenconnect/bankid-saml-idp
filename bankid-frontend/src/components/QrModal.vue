1820d482a986f33a2674d6d3f8319438af8c130f
Mattias Kesti <extern.mattias.kesti@digg.se>
2023-09-28 12:46:36 +0200

    Add QR modal component

---
+++ bankid-frontend/src/components/QrModal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import QrDisplay from './QrDisplay.vue';
+  import QrInstructions from './QrInstructions.vue';
+
+  const props = defineProps<{
+    image: string;
+    size: string;
+  }>();
+</script>
+
+<template>
+  <QrDisplay :image="props.image" :size="props.size" />
+  <QrInstructions></QrInstructions>
+  <form action="dialog">
+    <button class="btn-default">Stäng</button>
+  </form>
+</template>
+
+<style scoped>
+  form {
+    text-align: center;
+  }
+</style>
