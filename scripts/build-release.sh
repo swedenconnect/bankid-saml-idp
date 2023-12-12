989ab67a4776d7fc782edfa6c32a1cf207288650
Martin Lindström <martin.lindstrom@litsec.se>
2023-12-12 15:06:35 +0100

    1.1.2

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -8,7 +8,8 @@ usage() {
     echo "Usage: $0 [options...]" >&2
     echo
     echo "   -v      Version of BankID IdP to deploy"
-    echo "   -c      Make GIT add and check-in of updated apidocs."
+    echo "   -c      Make GIT add and check-in of updated apidocs"
+    echo "   -d      Publish Docker image to GitHub Container registry"
     echo "   -h      Prints this help"
     echo
 }
@@ -17,6 +18,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VERSION=""
 CHECK_IN_FLAG=false
+PUBLISH_IMAGE_FLAG=false
+export DOCKER_REPO=ghcr.io
 
 while :
 do
@@ -31,6 +34,10 @@ do
       ;;
   -c)
       CHECK_IN_FLAG=true
+      shift 1
+      ;;
+  -d)
+      PUBLISH_IMAGE_FLAG=true
       shift 1
       ;;
   -*)
@@ -50,7 +57,21 @@ if [ "$VERSION" == "" ]; then
     exit 1
 fi
 
-pushd ${SCRIPT_DIR}/.. > /dev/null
+if [ "$PUBLISH_IMAGE_FLAG" == true ]; then
+    if [ -z "$GITHUB_USER" ]; then
+      echo "The GITHUB_USER variable must be set"
+      exit 1
+    fi
+
+    if [ -z "$GITHUB_ACCESS_TOKEN" ]; then
+      echo "The GITHUB_ACCESS_TOKEN variable must be set"
+      exit 1
+    fi
+    
+    echo "Logging in to ${DOCKER_REPO} ..."
+    echo $GITHUB_ACCESS_TOKEN | docker login $DOCKER_REPO -u $GITHUB_USER --password-stdin    
+    
+fi
 
 echo "Building Javadoc ..."
 mvn clean javadoc:aggregate -Prelease
@@ -64,18 +85,12 @@ fi
 
 echo "Building BankID IdP and BankID RP API ..."
 
-mvn clean install -Prelease
-mvn deploy -Prelease
-mvn nexus-staging:release -Prelease
+mvn -f ${SCRIPT_DIR}/../pom.xml clean install -Prelease
+mvn -f ${SCRIPT_DIR}/../pom.xml deploy -Prelease
 
-# echo "Building BankID IdP without frontend ..."
-# popd > /dev/null
-# pushd ${SCRIPT_DIR}/../bankid-idp > /dev/null
-
-# mvn clean deploy -Prelease -Dbackend-only
-# mvn nexus-staging:release -Prelease -Dbackend-only
-
-popd > /dev/null
+if [ "$PUBLISH_IMAGE_FLAG" == true ]; then
+  mvn -f ${SCRIPT_DIR}/../bankid-idp/pom.xml jib:build -Djib.to.tags=${VERSION},latest
+fi
 
 echo "Version ${VERSION} was successfully deployed"
 echo "Remember to tag your release!"
