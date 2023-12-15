5a5fffa56452ec37bd712e4389f670a2fc20458f
Martin Lindström <martin.lindstrom@litsec.se>
2023-12-15 18:58:25 +0100

    Bumped to 1.1.3-SNAPSHOT

--- bankid-idp/src/main/java/se/swedenconnect/bankid/idp/ApplicationVersion.java
+++ bankid-idp/src/main/java/se/swedenconnect/bankid/idp/ApplicationVersion.java
@@ -25,7 +25,7 @@ public final class ApplicationVersion {
 
   private static final int MAJOR = 1;
   private static final int MINOR = 1;
-  private static final int PATCH = 2;
+  private static final int PATCH = 3;
 
   /**
    * Global serialization value for classes.
